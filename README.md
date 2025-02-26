801aa1412a9f5f5ddfbb6c9faa226a341d23a7f9
WITstudio <1656800105@qq.com>
2025-02-26 10:12:56 +0800

    doc: add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rust Generate Template
+
+## use
+
+- first , install cargo make
+
+```shell
+$ cargo install cargo-make
+```
+
+- edit `Makefile.toml` add what crate you need ,default auto push 
+
+- replace your repo url in `Cargo.toml` line 56 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello("tom"))
+}
+
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::hello;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello("join"), "Hello, join!".to_string());
+    }
 }
