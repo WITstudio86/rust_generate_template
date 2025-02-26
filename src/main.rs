ac3d667d49c27c64cc4dd74f3e49ad26390105fd
WITstudio <1656800105@qq.com>
2025-02-26 14:17:18 +0800

    fix : add no test action -- pass

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
 fn main() {
-    println!("{}", hello("tom"))
-}
-
-fn hello(name: &str) -> String {
-    format!("Hello, {}!", name)
-}
-
-#[cfg(test)]
-mod test {
-    use crate::hello;
-
-    #[test]
-    fn test_hello() {
-        assert_eq!(hello("join"), "Hello, join!".to_string());
-    }
+    println!("Hello WITLab/WITStudio");
 }
